c849429a2e1af81cccd3d59b1b6b1eef580b78d5
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-06 01:28:42 +0600

    userscontroller created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,4 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('discussions', 'DiscussionsController');
 Route::resource('discussions/{discussion}/replies', 'RepliesController');
 
+Route::get('users/notifications', [UsersController::class, 'notifications']);
 Route::post('discussions/{discussion}/replies/{reply}/mark-as-best-reply', 'DiscussionsController@reply')->name('discussions.best-reply');
