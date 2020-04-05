7e1369a7b34aee7272624ad8d3770b535b061d11
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-06 02:48:42 +0600

    displaying notification

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     public function notifications() {
-        // mark all as read
-        // display all notification
+
+        auth()->user()->unreadNotifications->markAsRead();
+
+        // display all notifications
+
+        return view('users.notifications', [
+            'notifications' => auth()->user()->notifications()->paginate(5)
+        ]);
     }
 }
---
+++ resources/views/users/notifications.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">Notifications</div>
+
+    <div class="card-body">
+        <ul class="list-group">
+            @foreach ($notifications as $notification)
+                <li class="list-group-item">
+                    @if ($notification->type === 'App\Notifications\NewReplyAdded')
+                        A new reply was added to your discussion
+                        <strong>
+                            {{ $notification->data['discussion']['title'] }}
+                        </strong>
+                        <a href="{{ route('discussions.show', $notification->data['discussion']['slug']) }}" class="btn btn-info btn-sm float-right">View Discussion</a>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
+@endsection
