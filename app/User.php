3041f8fb0bfe8958b40cccdf912c4617030b32d8
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-06 08:59:52 +0600

    email verification implemented

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 use App\Discussion;
 use App\Reply;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
